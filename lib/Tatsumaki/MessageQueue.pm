17c5dd57708f151eeaeaaae1ab38f9e75abf9620
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-11-12 19:49:44 -0800

    s/session/client/g

--- lib/Tatsumaki/MessageQueue.pm
+++ lib/Tatsumaki/MessageQueue.pm
@@ -6,9 +6,9 @@ use Scalar::Util;
 use Time::HiRes;
 use constant DEBUG => $ENV{TATSUMAKI_DEBUG};
 
-has channel  => (is => 'rw', isa => 'Str');
-has backlog  => (is => 'rw', isa => 'ArrayRef', default => sub { [] });
-has sessions => (is => 'rw', isa => 'HashRef', default => sub { +{} });
+has channel => (is => 'rw', isa => 'Str');
+has backlog => (is => 'rw', isa => 'ArrayRef', default => sub { [] });
+has clients => (is => 'rw', isa => 'HashRef', default => sub { +{} });
 
 our $BacklogLength = 30; # TODO configurable
 
@@ -33,87 +33,87 @@ sub append_backlog {
 sub publish {
     my($self, @events) = @_;
 
-    for my $sid (keys %{$self->sessions}) {
-        my $session = $self->sessions->{$sid};
-        if ($session->{cv}->cb) {
+    for my $client_id (keys %{$self->clients}) {
+        my $client = $self->clients->{$client_id};
+        if ($client->{cv}->cb) {
             # currently listening: flush and send the events right away
-            my @ev = (@{$session->{buffer}}, @events);
-            $self->flush_events($sid, @ev);
+            my @ev = (@{$client->{buffer}}, @events);
+            $self->flush_events($client_id, @ev);
         } else {
             # between long poll comet: buffer the events
             # TODO: limit buffer length
-            warn "Buffering new events for $sid" if DEBUG;
-            push @{$session->{buffer}}, @events;
+            warn "Buffering new events for $client_id" if DEBUG;
+            push @{$client->{buffer}}, @events;
         }
     }
     $self->append_backlog(@events);
 }
 
 sub flush_events {
-    my($self, $sid, @events) = @_;
+    my($self, $client_id, @events) = @_;
 
-    my $session = $self->sessions->{$sid} or return;
+    my $client = $self->clients->{$client_id} or return;
     try {
-        my $cb = $session->{cv}->cb;
-        $session->{cv}->send(@events);
-        $session->{cv} = AE::cv;
-        $session->{buffer} = [];
+        my $cb = $client->{cv}->cb;
+        $client->{cv}->send(@events);
+        $client->{cv} = AE::cv;
+        $client->{buffer} = [];
 
-        if ($session->{persistent}) {
-            $session->{cv}->cb($cb);
+        if ($client->{persistent}) {
+            $client->{cv}->cb($cb);
         } else {
-            $session->{timer} = AE::timer 30, 0, sub {
+            $client->{timer} = AE::timer 30, 0, sub {
                 Scalar::Util::weaken $self;
-                warn "Sweep $sid (no long-poll reconnect)" if DEBUG;
-                undef $session;
-                delete $self->sessions->{$sid};
+                warn "Sweep $client_id (no long-poll reconnect)" if DEBUG;
+                undef $client;
+                delete $self->clients->{$client_id};
             };
         }
     } catch {
         /Tatsumaki::Error::ClientDisconnect/ and do {
-            warn "Client $sid disconnected" if DEBUG;
-            undef $session;
-            delete $self->sessions->{$sid};
+            warn "Client $client_id disconnected" if DEBUG;
+            undef $client;
+            delete $self->clients->{$client_id};
         };
     };
 }
 
 sub poll_once {
-    my($self, $sid, $cb, $timeout) = @_;
+    my($self, $client_id, $cb, $timeout) = @_;
 
     my $is_new;
-    my $session = $self->sessions->{$sid} ||= do {
+    my $client = $self->clients->{$client_id} ||= do {
         $is_new = 1;
         + { cv => AE::cv, persistent => 0, buffer => [] };
     };
 
-    $session->{cv}->cb(sub { $cb->($_[0]->recv) });
+    $client->{cv}->cb(sub { $cb->($_[0]->recv) });
 
     # reset garbage collection timeout with the long-poll timeout
-    $session->{timer} = AE::timer $timeout || 55, 0, sub {
+    $client->{timer} = AE::timer $timeout || 55, 0, sub {
         Scalar::Util::weaken $self;
-        warn "Timing out $sid long-poll" if DEBUG;
-        $self->flush_events($sid);
+        warn "Timing out $client_id long-poll" if DEBUG;
+        $self->flush_events($client_id);
     };
 
-    # flush backlog for a new session
+    # flush backlog for a new client
     if ($is_new) {
         my @events = $self->backlog_events;
-        $self->flush_events($sid, @events) if @events;
+        $self->flush_events($client_id, @events) if @events;
     }
 }
 
 sub poll {
-    my($self, $sid, $cb) = @_;
+    my($self, $client_id, $cb) = @_;
 
     my $cv = AE::cv;
     $cv->cb(sub { $cb->($_[0]->recv) });
-    my $s = $self->sessions->{$sid} = {
+    my $s = $self->clients->{$client_id} = {
         cv => $cv, persistent => 1, buffer => [],
     };
 
     my @events = $self->backlog_events;
-    $self->flush_events($sid, @events) if @events;
+    $self->flush_events($client_id, @events) if @events;
 }
 
 1;
