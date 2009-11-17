18e61dfe08e56ff171e9fcd50a5e06473c787e33
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-11-16 21:37:43 -0800

    Revert "use Text::MicroTemplate::Extended by default"
    This reverts commit 7801879b345ccdf2f677ab299dec12ae0f0fd168.

--- lib/Tatsumaki/Template/Micro.pm
+++ lib/Tatsumaki/Template/Micro.pm
@@ -2,19 +2,18 @@ package Tatsumaki::Template::Micro;
 use Moose;
 extends 'Tatsumaki::Template';
 
-use Text::MicroTemplate::Extended;
-has mt => (is => 'rw', isa => 'Text::MicroTemplate::Extended', handles => [ 'render_file' ]);
+use Text::MicroTemplate::File;
+has mt => (is => 'rw', isa => 'Text::MicroTemplate::File', handles => [ 'render_file' ]);
 
 sub BUILD {
     my $self = shift;
 
-    my $mt = Text::MicroTemplate::Extended->new(
+    my $mt = Text::MicroTemplate::File->new(
         include_path => [ 'templates' ],
         use_cache => 0,
         tag_start => '<%',
         tag_end   => '%>',
         line_start => '%',
-        extension => '',
     );
 
     $self->mt($mt);
